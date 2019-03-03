12ea193e3935a8dfabe295739fb8d7142aba9dc7
QBnewb <37447069+QBnewb@users.noreply.github.com>
2019-03-03 21:49:27 +0800

    Revert "Revert "。。。""
    This reverts commit 025513a438aae36d2109c434f2d674990e2fc685.

---
+++ src/com/mubu2/TimerHandler.java
@@ -0,0 +1,29 @@
+package com.mubu2;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+
+import javax.swing.Action;
+import javax.swing.JLabel;
+
+public class TimerHandler implements ActionListener {
+	JLabel jLabel = new JLabel();
+	
+	public void setjLabel(JLabel jLabel) {
+		this.jLabel = jLabel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String text = "当前北京时间：";
+		long time = System.currentTimeMillis();
+		SimpleDateFormat clock = new SimpleDateFormat("HH:mm:ss");
+		text += clock.format(new Date(time));
+		jLabel.setText(text);
+	}
+}
